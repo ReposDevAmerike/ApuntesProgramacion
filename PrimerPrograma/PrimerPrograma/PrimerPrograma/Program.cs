f3655d4a1472944817a1d609662b326697834eff
Sol Zavala <cdmx3310@amerike.edu.mx>
2025-08-19 18:34:14 -0600

    Add files via upload
    el primer programa que hicimos

---
+++ PrimerPrograma/PrimerPrograma/PrimerPrograma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1: Despertar");
+            Console.WriteLine("2: Veo la hora");
+            Console.WriteLine("3: Me levanto");
+            Console.WriteLine("4: Me baño ");
+            Console.WriteLine("5: Me cambio");
+            Console.WriteLine("6: Desayuno");
+            Console.WriteLine("7: Juego Lol");
+            Console.WriteLine("8: Voy a la uni");
+            Console.WriteLine("9: Regreso a mi casa");
+            Console.WriteLine("10: Anoto cosas que no me acordaba de estudios");
+        }
+        
+    }
+}
