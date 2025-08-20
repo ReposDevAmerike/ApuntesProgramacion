bd7eaab815ee5c6125160297517bdbf91a656562
Sol Zavala <cdmx3310@amerike.edu.mx>
2025-08-19 18:33:03 -0600

    Add files via upload

---
+++ InputUser/InputUser/InputUser/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            int edad;
+            string telefono;
+            int postal;
+            string direccion;
+            bool isint = false;
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su Apellido");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su telefono");
+            telefono = Console.ReadLine();
+            Console.WriteLine("Ingrese su Codigo Postal");
+            postal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su direccion:");
+            direccion = Console.ReadLine();
+
+
+        }
+    }
+}
